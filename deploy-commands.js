ecc0f9d8e098e32e31d75384a3de42efb8afe059
NTX8205 <ad0124578963@gmail.com>
2022-06-13 11:55:42 +0800

    update

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,7 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
+
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
@@ -13,6 +14,7 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	commands.push(command.data.toJSON());
 }
+
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
